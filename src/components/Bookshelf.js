eea41e2d28594cf0ac739f2bfabc7d4e9410d0df
vijaylaxmi hanamshetty <vijaylaxmihanamshetty38@.in>
2024-06-06 17:38:49 +0530

    first commit

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -3,7 +3,7 @@ import BookCard from "./BookCard";
 
 const Bookshelf = ({ books, actionLabel, onAction }) => {
   return (
-    <div className="  px-10 md:px-20 lg:px-30 py-10">
+    <div className="  px-4 md:px-20 lg:px-30 py-10">
       <div className=" grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         {books.map((book) => (
           <BookCard
